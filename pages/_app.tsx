d576811493b48672c9eb073930722a407b7e467f
geekysam7 <geekysam7@gmail.com>
2024-01-17 22:12:42 +0530

    rm refetch on window focus

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,16 @@ import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false, // default: true
+          },
+        },
+      })
+  );
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
