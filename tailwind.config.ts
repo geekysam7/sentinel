20791ee4541c79bcb2a2a89cc82d2bade06c27e3
geekysam7 <geekysam7@gmail.com>
2024-01-17 21:28:13 +0530

    update packages, add theme, add logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        success: "#2d6f41",
+        danger: "#9C2B41",
+        // main bg
+        mainBg: "#090a10",
+        // card
+        bgCardPrimary: "#131721",
+        bgCardSecondary: "#0E111B",
+        bgCardTertiary: "#221E27",
+        bgCardSidebar: "#161723",
+        bgCardSidebarItem: "#11131c",
+        // list
+        bgListPrimary: "#10121B",
+        // theme
+        primary: "#7D99E1",
+        secondary: "#4371EB",
+        tertiary: "#1E5FFF",
+        // text
+        textPrimary: "#FFFFFF",
+        textSecondary: "#737478",
+        textTertiary: "#7D99E1",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
