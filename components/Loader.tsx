73027a839e37f1073a090d6acee0a8ea1f681f6b
geekysam7 <geekysam7@gmail.com>
2024-01-20 04:09:23 +0530

    fix loader padding

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -6,21 +6,23 @@ import { SESSION_AGGREGATION } from "@/constants/sessonHistory";
 const Loader = () => {
   return (
     <div className="flex justify-between w-full">
-      <div className="animate-pulse hidden p-4 xl:flex flex-col rounded-lg w-96 min-w-96 mt-8 mr-4 border border-gray-800">
-        <div className="flex flex-col w-full h-24">
-          <div className="h-6 w-full bg-bgCardTertiary rounded-md"></div>
-          <div className="h-6 w-full bg-bgCardTertiary rounded-md mt-4"></div>
-        </div>
-        <div className="w-full">
-          {INSTANCE_ACTIVITY_LIST.map(({ label }) => (
-            <div
-              key={label}
-              className="h-8 mt-4 w-full bg-bgCardTertiary rounded-md"
-            ></div>
-          ))}
+      <div>
+        <div className="animate-pulse hidden p-4 xl:flex flex-col rounded-lg w-96 min-w-96 mt-8 mr-4 border border-gray-800">
+          <div className="flex flex-col w-full h-24">
+            <div className="h-6 w-full bg-bgCardTertiary rounded-md"></div>
+            <div className="h-6 w-full bg-bgCardTertiary rounded-md mt-4"></div>
+          </div>
+          <div className="w-full">
+            {INSTANCE_ACTIVITY_LIST.map(({ label }) => (
+              <div
+                key={label}
+                className="h-8 mt-4 w-full bg-bgCardTertiary rounded-md"
+              ></div>
+            ))}
+          </div>
         </div>
       </div>
-      <div className="shadow rounded-md p-4 w-full flex flex-col">
+      <div className="shadow rounded-md w-full flex flex-col pt-4">
         <div className="w-full pt-4">
           <h2 className="text-3xl">Session History</h2>
           <ul className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-4 gap-4 w-full animate-pulse">
